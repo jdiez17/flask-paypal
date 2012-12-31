c555e504a656d933107d15ff697476e2ef99f23e
jdiez17 <jose.manuel.diez@gmail.com>
2012-12-31 17:52:05 +0100

    Added README

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ def paypal_cancel():
     return redirect(url_for('index'))
 
 if __name__ == '__main__': 
-    app.run(host='0.0.0.0', port=8338, debug=True)
+    app.run(host='127.0.0.1', port=8338, debug=True)
